4309dfccd99aafa4c1808845172ed5ded54adafd
Klodi <k.pepaj@hotmail.it>
2022-10-04 10:36:04 +0200

    added lock screen keybinding

--- dwm-6.3/config.h
+++ dwm-6.3/config.h
@@ -58,18 +58,20 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-e", "tmux", NULL };
 static const char *web[] = { "brave-browser", NULL };
 /* vc must be a script present in /usr/local/bin */
 static const char *volumeup[] = {"vc", "up", NULL};  /* vc (volume control) is a script i wrote to handle the volume */
 static const char *volumedown[] = {"vc", "down", NULL}; /* vc (volume control) is a script i wrote to handle the volume */
 static const char *mute[] = {"vc", "toggle", NULL}; /* vc (volume control) is a script i wrote to handle the volume */
+static const char *lock[] = {"slock", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = web } },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lock } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
