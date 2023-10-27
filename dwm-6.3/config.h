d9996b8a2c4964cebffc0d59d92c98cb0c9981ea
Klodi <k.pepaj@hotmail.it>
2023-10-28 01:53:58 +0200

    installed to arch linux

--- dwm-6.3/config.h
+++ dwm-6.3/config.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *web[] = { "brave-browser", NULL };
+static const char *web[] = { "brave", NULL };
 static const char *poweroff[] = { "shutdown", "now" };
 /* vc must be a script present in /usr/local/bin */
 static const char *volumeup[] = {"vc", "up", NULL};  /* vc (volume control) is a script i wrote to handle the volume */
