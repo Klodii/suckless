9bc14bea04a9cf9ac6eb716dfeab75dd8da0ccd7
Klodii <k.pepaj@hotmail.it>
2025-06-20 09:08:14 +0200

    feat(dwm): set brave as the web browser

--- slock-1.4/config.h
+++ slock-1.4/config.h
@@ -1,5 +1,5 @@
 /* user and group to drop privileges to */
-static const char *user  = "klod";
+static const char *user  = "klodi";
 static const char *group = "users";
 
 static const char *colorname[NUMCOLS] = {
