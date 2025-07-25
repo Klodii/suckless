e1f991168c2cb2cbba2ad308445184c2a0f05a39
Klodii <k.pepaj@hotmail.it>
2025-07-25 08:35:46 +0200

    feat(config.h): change browser from brave to zen-browser

--- dwm-6.3/config.h
+++ dwm-6.3/config.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *web[] = { "brave", NULL };
+static const char *web[] = { "zen-browser", NULL };
 static const char *poweroff[] = { "shutdown", "now" };
 static const char *restart[] = { "shutdown", "-r", "now" };
 /* vc must be a script present in /usr/local/bin */
